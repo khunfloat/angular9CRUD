e9bad213924593eff8617437d1ce6e5fad74730d
khunfloat <khunfloat@gmail.com>
2020-04-11 18:39:48 +0700

    first code

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MemberService } from './member.service';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MemberAddComponent } from './member-add/member-add.component';
+import { MemberGetComponent } from './member-get/member-get.component';
+import { MemberEditComponent } from './member-edit/member-edit.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MemberAddComponent,
+    MemberGetComponent,
+    MemberEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SlimLoadingBarModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [
+    MemberService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
